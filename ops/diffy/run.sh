e1ffcdd623a506dc4d74f86410cbcd3dec99d61a
DossIn01 <dossin01@phxdossin01-mac.home>
2021-06-01 11:31:47 +0100

    running services and diffy through a Docker compose

--- ops/diffy/run.sh
+++ ops/diffy/run.sh
@@ -1,6 +1,3 @@
-dotnet restore ./Current.Web.Api
-dotnet build ./Current.Web.Api
-
 docker build -t same-same-but-diffy-image -f ops/Dockerfile .
 
-docker-compose up
+docker-compose -f ops/diffy/docker-compose.yml up
