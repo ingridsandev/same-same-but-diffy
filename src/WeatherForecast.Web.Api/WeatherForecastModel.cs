ae2d3ad6db0ddf7f8bf50650d3d04b37727339aa
DossIn01 <dossin01@phxdossin01-mac.home>
2021-06-03 10:06:48 +0100

    cleaning up

--- src/WeatherForecast.Web.Api/WeatherForecastModel.cs
+++ src/WeatherForecast.Web.Api/WeatherForecastModel.cs
@@ -11,7 +11,5 @@ namespace WeatherForecast.Web.Api
         public int TemperatureF => 32 + (int) (TemperatureC / 0.5556);
 
         public string Summary { get; set; }
-
-        public string Test {get; set;}
     }
 }
